427e8e3797993a37c565b41707c9606a6724ce6a
Oliwer Sobolewski <oliwer.sobolewski@nask.pl>
2024-02-26 22:42:20 +0100

    Fix install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git submodule add https://github.com/0NG/Format-Preserving-Encryption.git dependencies/Format-Preserving-Encryption &&
+git submodule update --init &&
 ./gen_test_keys.sh &&
 cd dependencies/Format-Preserving-Encryption/ &&
 make &&
